6515337783e16151611a80a9f859472ef0310d2d
Zbigniew Gawroński <zbigniew.gawronski@microsoft.wsei.edu.pl>
2021-05-09 19:30:48 +0200

    MainContainer links

--- src/components/MainPage/MainContainer.tsx
+++ src/components/MainPage/MainContainer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { Colors } from '../../styledHelpers/Colors';
@@ -48,6 +48,7 @@ const BlockImgPlace = styled.div`
 
 const BlockImg = styled.img`
     position: relative;
+    width: 50px;
     margin: 5px;
     border-radius: 3px;
 `;
@@ -86,6 +87,60 @@ const BlockF = styled.a`
 `;
 
 export const MainContainer: FC = () => {
+    const postId: number = 3;
+
+    const [title, setTitle] = useState<any>([]);
+    const [image, setImage ] = useState<any>([]);
+
+    const [userId, setUserID] = useState<any>(10);
+    const [userName, setUserName] = useState<any>([]);
+    const [userImage, setUserImage] = useState<any>([]);
+
+    useEffect(() => {
+        async function getInfo(postID: number) {
+            try{
+                const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postID}`);
+                const data = await response.json();
+                const title = JSON.stringify(data.title).slice(1,-1);
+                const titleFirstLetterUpper = title.charAt(0).toUpperCase() + title.slice(1);
+                setUserID(data.userId);
+
+                const responseUrl = await fetch(`https://jsonplaceholder.typicode.com/photos/${postID}`);
+                const dataUrl = await responseUrl.json();
+                const url = JSON.stringify(dataUrl.url).slice(1,-1);
+
+                const responseUser = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+                const dataUser = await responseUser.json();
+
+                const responseUrlIcon = await fetch(`https://jsonplaceholder.typicode.com/photos/${userId}`);
+                const dataUrlIcon = await responseUrlIcon.json();
+
+
+                setTitle((arr: any) => ([...arr, titleFirstLetterUpper]));
+                setImage((arr: any) => ([...arr, url]));
+
+                if(dataUser.name != null){
+                    const userName = JSON.stringify(dataUser.name).slice(1,-1);
+                    setUserName((arr: any) => ([...arr, userName]));
+                }
+
+                if(dataUrlIcon.url != null){
+                    const urlIcon = JSON.stringify(dataUrlIcon.url).slice(1,-1);
+                    setUserImage(urlIcon);
+                }
+            }catch(e){
+
+            }
+
+        }
+        try{
+            getInfo(postId);
+            getInfo(5);
+            getInfo(6);
+        } catch(e){}
+
+    });
+
     return (
         <Wrapper4>
             <BlockImgPlace>
@@ -96,21 +151,21 @@ export const MainContainer: FC = () => {
             <MainText>
                 <MainTitle>Latest publications</MainTitle>
                 <MainBlocks>
-                    <BlockImg src="./media/hands.png" alt=""/>
+                    <BlockImg src={image[0]} alt=""/>
                     <BlockTxt>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit... and one more line for the demo</p>
                         <BlockF href="">7 jan 2020 Img JohnDoe  </BlockF>
                     </BlockTxt>
                 </MainBlocks>
                 <MainBlocks>
-                    <BlockImg src="./media/hands.png" alt=""/>
+                    <BlockImg src={image[1]} alt=""/>
                     <BlockTxt>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit... and one more line for the demo</p>
                             <BlockF href="">7 jan 2020 Img JohnDoe </BlockF>
                     </BlockTxt>
                 </MainBlocks>
                 <MainBlocks>
-                    <BlockImg src="./media/hands.png" alt=""/>
+                    <BlockImg src={image[2]} alt=""/>
                     <BlockTxt>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit... and one more line for the demo</p>
                         <BlockF href="">7 jan 2020 Img JohnDoe  </BlockF>
