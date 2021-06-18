ae6f864dbb5354199404636510453d859d0e4aa4
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-06-18 20:17:43 +0200

    static profile rdy

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -47,10 +47,17 @@ const DivIcons = styled.div`
         height: 20px;
         padding: 5px
     }
-    span {
-        padding: 5px;
 
+    button{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        height: 30px;
+        margin: 0 5px;
+        background-color: transparent;
+        border: none;
     }
+
 `;
 
 const DivProfile = styled.div`
@@ -191,7 +198,6 @@ const DivProposals = styled.div`
     padding: 5px;
     .tabelka{
         display: flex;
-        justify-content: space-between;
         flex-wrap: wrap;
         column-count: 6;
         column-width: 16%;
@@ -221,6 +227,28 @@ const DivProposals = styled.div`
 
 `;
 
+const DivAmount = styled.div`
+    padding: 0 10px 10px 10px;
+    margin-top: -5px;
+    h2{
+        font-weight: 600;
+        margin-bottom: 10px;
+        margin-top: 15px;
+    }
+
+
+    table > tr > td {
+        padding: 5px 0 5px 0;
+    }
+    table{
+        width: 100%;
+    }
+
+
+`;
+
+
+
 export const Profile: FC = () => {
     const { someImg, usersList } = useSelector<IState, IUsersReducer>(state => ({
         ...state.users
@@ -245,12 +273,9 @@ export const Profile: FC = () => {
         <WrapperProfile>
             <div className="hr">
                 <DivIcons>
-                    <img src="./media/icons/chat.png" alt="" />
-                    <span>Message</span>
-                    <img src="./media/icons/request.png" alt="" />
-                    <span>Create a request</span>
-                    <img src="./media/icons/cluster.png" alt="" />
-                    <span>Add to a cluster</span>
+                    <button><img src="./media/icons/chat.png" alt="" />Message</button>
+                    <button><img src="./media/icons/request.png" alt="" />Create a request</button>
+                    <button><img src="./media/icons/cluster.png" alt="" />Add to a cluster</button>
                     <StyledLink to="/">
                         <XSpan>X</XSpan>
                     </StyledLink>
@@ -357,6 +382,65 @@ export const Profile: FC = () => {
                     <span>See more proposals</span>
                 </DivProposals>
             </div>
-        </WrapperProfile>
+            <div className="hr">
+                <DivProposals>
+                    <h2>Internal reviews</h2>
+                    <div className="tabelka">
+                        <h3>Name</h3>
+                        <h3>Entity</h3>
+                        <h3>Location</h3>
+                        <h3>Expertise</h3>
+                        <h3>Date</h3>
+                    </div>
+                </DivProposals>
+            </div>
+            <div className="hr">
+                <DivProposals>
+                    <div className="tabelka">
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+                        <h4>asdfasdf</h4>
+
+                    </div>
+                </DivProposals>
+                <h3>See more Reviews</h3>
+            </div>
+            <DivAmount>
+                <h2>Amount of fees</h2>
+                <table>
+                    <tr>
+                        <td>Year</td> <td>Cost center</td> <td>Total amount</td> <td>Law firm</td>
+                    </tr>
+                    <tr>
+                        <td>2019</td> <td>CS 153</td> <td> 3 500€</td> <td>Clifford chance</td>
+                    </tr>
+                    <tr>
+                        <td>2018</td> <td>CS 153</td> <td> 9 500€</td> <td>Linklaters</td>
+                    </tr>
+                    <tr>
+                        <td>2017</td> <td>CS 47</td> <td>10 500€</td> <td>Linklaters</td>
+                    </tr>
+                    <tr>
+                        <td></td> <td>CS 153</td> <td>18 500€</td> <td>Linklaters</td>
+                    </tr>
+                    <tr>
+                        <td></td> <td>CS 32</td> <td>15 500€</td> <td>Linklaters</td>
+                    </tr>
+                </table>
+
+            </DivAmount>
+        </WrapperProfile >
     );
 };
